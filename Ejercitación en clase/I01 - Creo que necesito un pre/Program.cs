b43ab90a530e579a0a0a85e87cb95f49ba8dc18d
Yukio <yukikreczmer@gmail.com>
2022-08-25 11:15:03 -0300

    Clase 02

---
+++ Ejercitación en clase/I01 - Creo que necesito un pre/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace I01___Creo_que_necesito_un_pre_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Crear una aplicación de consola y una biblioteca de clases que contenga la clase Cuenta.
+
+            Deberá tener los atributos:
+
+            titular que contendrá la razón social del titular de la cuenta.
+            cantidad que será un número decimal que representa al monto actual de dinero en la cuenta.
+            Construir los siguientes métodos para la clase:
+
+            Un constructor que permita inicializar todos los atributos.
+            Un método getter para cada atributo.
+            mostrar retornará una cadena de texto con todos los datos de la cuenta.
+            ingresar recibirá un monto para acreditar a la cuenta. Si el monto ingresado es negativo, no se hará nada.
+            retirar recibirá un monto para debitar de la cuenta. La cuenta puede quedar en negativo.
+            En el método Main, simular depósitos y extracciones de dinero de la cuenta, e ir mostrando como va variando el saldo.*/
+            
+            Cuenta cuenta1 = new Cuenta("Roberto");
+            Cuenta.MostrarDatos(cuenta1);
+            Cuenta.IngresarDatos(cuenta1, 1000);
+            Console.WriteLine("Se ingresaron $1000");
+            Cuenta.MostrarDatos(cuenta1);
+            Cuenta.RetirarMonto(cuenta1, 500);
+            Console.WriteLine("Se retiraron $500");
+            Cuenta.MostrarDatos(cuenta1);
+
+
+            Console.WriteLine();
+            // Console.WriteLine($"El valor máximo ingresado es: {maxNumeroIngresado}");
+        }
+    }
+}
---
+++ Ejercitación en clase/I02 - Error al cubo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace I02___Error_al_cubo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Ingresar un número y mostrar el cuadrado y el cubo del mismo. 
+             * Se debe validar que el número sea mayor que cero, caso contrario, mostrar el mensaje: "ERROR. ¡Reingresar número!".*/
+            string numeroIngresadoStr;
+            int numeroEscalar;
+            bool sePudoParsearAInt = true;
+
+            int cuadradoDelNumero;
+            int cuboDelNumero;
+            do
+            {
+                if (!sePudoParsearAInt)
+                {
+                    Console.WriteLine("ERROR. ¡Reingresar número!");
+                }
+                else
+                {
+                    Console.WriteLine("Ingrese un numero: ");
+                }
+                numeroIngresadoStr = Console.ReadLine();
+                sePudoParsearAInt = int.TryParse(numeroIngresadoStr, out numeroEscalar);
+                //si me ingresa un numero decimal con "." va bien, si lo hace con "," lo toma entero → (10,10 seria 1010)                    
+                //podria validar que no ingrese puntos
+            } while (!sePudoParsearAInt);
+            cuadradoDelNumero = numeroEscalar;
+            cuboDelNumero = numeroEscalar;
+            for (int i = 0; i < 2; i++)
+            {
+                cuadradoDelNumero = cuadradoDelNumero * numeroEscalar
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
